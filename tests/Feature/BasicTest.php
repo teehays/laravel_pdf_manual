43fef24f7606e9b27b4070ac88892ad2137e7a7d
Tytn Hays <Tytn@Tytn.org.uk>
2018-10-10 23:16:14 +0100

    Added another test!

--- tests/Feature/BasicTest.php
+++ tests/Feature/BasicTest.php
@@ -16,5 +16,14 @@ class BasicTest extends TestCase
         $this->assertTrue($box->has('toy'));
         $this->assertFalse($box->has('ball'));
     }
+    public function testTakeOneFromTheBox()
+    {
+        $box = new Box(['torch']);
+
+        $this->assertEquals('torch', $box->takeOne());
+
+        // Null, now the box is empty
+        $this->assertNull($box->takeOne());
+    }
 
 }
